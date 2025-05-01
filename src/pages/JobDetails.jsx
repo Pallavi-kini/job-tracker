2bbc771c3f4597ef3e3cd93ef62f13555d74f8ec
Pallavi-Kini <pallavikini5566@gmail.com>
2025-05-01 21:39:29 +0530

    installed mantine and tailwind

---
+++ src/pages/JobDetails.jsx
@@ -0,0 +1,13 @@
+import { Button } from '@mantine/core'
+import React from 'react'
+
+const JobDetails = () => {
+  return (
+    <div><Button type="button" variant="filled" color="blue">Button</Button>
+      <h1 class="text-3xl font-bold underline text-blue-500">
+        Hello world!
+      </h1></div>
+  )
+}
+
+export default JobDetails
